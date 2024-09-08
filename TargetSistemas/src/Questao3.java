1987c92500a58a4a250296e145bb4d09397ba0fc
GabrielLM <biellucas.2015.glm@gmail.com>
2024-09-07 21:09:42 -0300

    Validação questão 3 finalizada

---
+++ TargetSistemas/src/Questao3.java
@@ -0,0 +1,12 @@
+public class Questao3 {
+    public static void main(String[] args) {
+        int indice = 12;
+        int soma = 0;
+        int k = 1;
+        while(k < indice){
+            k += 1;
+            soma += k;
+        }
+        System.out.println(soma);
+    }
+}
